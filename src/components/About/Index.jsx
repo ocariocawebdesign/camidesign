6fcb90dc2b7320bff84678a7b340c6c3a2bcd4a3
Carlos <ocariocawebdesign@gmail.com>
2022-11-29 12:29:43 -0300

    Colocação de videos

--- src/components/About/Index.jsx
+++ src/components/About/Index.jsx
@@ -3,6 +3,7 @@ import { HeroH1 } from "../HeroContent/styles";
 import LogoSreo from "../../assets/img/logoSREO.png";
 import LogoOndas from "../../assets/img/logoOndas.png";
 import LogoFabiola from "../../assets/img/fabiolaLogo.png";
+import Linix from "../../assets/videos/linix.mp4";
 
 export function About() {
   return (
@@ -18,7 +19,8 @@ export function About() {
       <br />
 
       <div className="row">
-        <AboutH1 className="animate__animated animate__fadeInLeft"
+        <AboutH1
+          className="animate__animated animate__fadeInLeft"
           style={{
             width: "100%",
             background: "#333",
@@ -27,11 +29,9 @@ export function About() {
             color: "#FAFAFA",
             borderRadius: "3px",
             padding: "1rem",
-            animation:'fadeInLeft',
-            animationDuration: '15s',
-            animateDelay: '10s'
-
-            
+            animation: "fadeInLeft",
+            animationDuration: "2s",
+            animateDelay: "10s",
           }}
         >
           some clients />
@@ -41,16 +41,19 @@ export function About() {
             style={{
               marginTop: "2rem",
               height: "220px",
-              padding: "1rem",
+              //padding: "1rem",
               margin: "0 auto",
               border: "none",
             }}
           >
-            <img
+            <div className="" style={{ marginLeft: "2rem", margin: "0 auto" }}>
+              <video src={Linix} controls width="250"></video>
+            </div>
+            {/*<img
               src={LogoSreo}
               className="img-fluid"
               style={{ margin: "0 auto" }}
-            />
+          />*/}
             <p>
               <strong></strong>
               <span style={{ color: "#979595" }}></span>
@@ -63,16 +66,19 @@ export function About() {
             style={{
               marginTop: "2rem",
               height: "220px",
-              padding: "1.5rem",
+              //padding: "1rem",
               margin: "0 auto",
               border: "none",
             }}
           >
-            <img
+            <div className="" style={{ marginLeft: "2rem", margin: "0 auto" }}>
+              <video src={Linix} controls width="250"></video>
+            </div>
+            {/*<img
               src={LogoOndas}
               className="img-fluid"
               style={{ margin: "0 auto" }}
-            />
+          />*/}
             <p>
               <strong></strong>
               <span style={{ color: "#979595" }}></span>
@@ -84,17 +90,20 @@ export function About() {
             style={{
               marginTop: "2rem",
               height: "220px",
-              padding: "1rem",
+              //padding: "1rem",
               margin: "0 auto",
               border: "none",
             }}
           >
-            <img
+            <div className="" style={{ marginLeft: "2rem", margin: "0 auto" }}>
+              <video src={Linix} controls width="250"></video>
+            </div>
+            {/*<img
               src={LogoFabiola}
               className="img-fluid"
               width="380"
               style={{ margin: "0 auto" }}
-            />
+          />*/}
             <p>
               <strong></strong>
               <span style={{ color: "#979595" }}></span>
@@ -106,12 +115,14 @@ export function About() {
             style={{
               marginTop: "2rem",
               height: "220px",
-              padding: "1rem",
+             // padding: "1rem",
               margin: "0 auto",
               border: "none",
             }}
           >
-            <img src={LogoSreo} className="img-fluid" />
+            <div className="" style={{ marginLeft: "2rem", margin: "0 auto" }}>
+              <video src={Linix} controls width="250"></video>
+            </div>
             <p>
               <strong></strong>
               <span style={{ color: "#979595" }}></span>
