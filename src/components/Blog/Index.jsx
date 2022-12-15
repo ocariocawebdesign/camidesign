81124c0636dadd0d6d3c91b03b9d3b9ecccb500e
Carlos <ocariocawebdesign@gmail.com>
2022-12-15 12:01:57 -0300

    Remoção dos estilos do blog para teste 2

--- src/components/Blog/Index.jsx
+++ src/components/Blog/Index.jsx
@@ -1,34 +1,35 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import parse from "html-react-parser";
-//import "./styles.css";
+import "./styles.css";
 
 //const url = "https://threeeyes.online/cami/wp-json/wp/v2/posts/";
-const url = "https://threeeyes.online/cami/wp-json/wp/v2/posts";
 
 export default function Blog() {
   const [posts, setPosts] = useState([]);
   const [totalPosts, SetTotalPosts] = useState(0);
-  const [showPosts, SetShowPosts] = useState(1);
+  const [showPosts, SetShowPosts] = useState(0);
 
   //const [autor, setAutor] = useState("");
+  const url = "https://threeeyes.online/cami/wp-json/wp/v2/posts";
 
-  useEffect(() => {
+  function getPosts() {
     axios
       .get(`${url}`)
 
-      .then(
-        (result) => {
-          //console.log(result.data);
-          //Posts que são impressos na tela
-          setPosts(result.data.slice(0, 4));
-          SetTotalPosts(result.data.length);
-          console.log(result.data.length);
-        },
+      .then((result) => {
+        //console.log(result.data);
+        //Posts que são impressos na tela
+        setPosts(result.data.slice(0, 4));
+        //SetShowPosts(posts)
+        SetTotalPosts(result.data.length);
+        console.log(result.data.length);
+      });
+  }
 
-        [showPosts]
-      );
-  });
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   function showMorePosts() {
     //setIsLoader(true);
@@ -50,11 +51,11 @@ export default function Blog() {
 
           return (
             <>
-              <div className="blog-posts">
-                <div className="box-content-posts">
-                  <div key={dados.id}>
+              <div key={dados.id}>
+                <div className="blog-posts">
+                  <div className="box-content-posts">
                     <h1>{dados.title.rendered}</h1>
-                    <p className="">{parse(`${dados.content.rendered}`)}</p>
+                    {parse(`${dados.content.rendered}`)}
                   </div>
                 </div>
               </div>
